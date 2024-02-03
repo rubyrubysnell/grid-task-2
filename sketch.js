6b83ecbd3be12d454c7b54bc14255fffd66a4913
rubyrubysnell <81196130+rubyrubysnell@users.noreply.github.com>
2024-02-03 14:50:50 +0000

    basic grid

--- sketch.js
+++ sketch.js
@@ -1,4 +1,23 @@
 function setup() {
-    createCanvas(400, 400);
-    background(255)
+
+    // colour options: blue, white, black, dark green, bright orange, soft pink, khaki, neon green, green
+    const colours = ["#82a3b4", "#e8e8e8", "#363638", "#f8a47b", "#f1b8a9", "#98b36c", "#899450"]
+
+    const cellNumber = 32
+    const cellSize = 25
+    const canvasSize = 800
+
+    createCanvas(canvasSize, canvasSize)
+
+    background(225)
+
+    for (let row = 0; row < cellNumber; row++) {
+        for (let column = 0; column < cellNumber; column++) {
+            noStroke()
+            fill(random(colours))
+            const xPosition = row * cellSize
+            const yPosition = column * cellSize
+            square(xPosition, yPosition, cellSize)
+        }
+    }
 }
